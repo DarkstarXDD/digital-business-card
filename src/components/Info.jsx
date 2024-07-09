41cb023e3bdeee02fe5f9c46c6312e945f0da21e
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-07-09 15:12:12 +0530

    separate into more components

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,3 +1,6 @@
+import About from "./About"
+import Interests from "./Interests"
+
 export default function Info() {
   return (
     <div className="info">
@@ -45,22 +48,8 @@ export default function Info() {
         </div>
       </div>
       <div className="descriptions flex-column">
-        <div className="description flex-column">
-          <h2 className="heading-secondary">About</h2>
-          <p className="text-secondary">
-            I am a frontend developer with a particular interest in making
-            things simple and automating daily tasks. I try to keep up with
-            security and best practices, and am always looking for new things to
-            learn.
-          </p>
-        </div>
-        <div className="description flex-column">
-          <h2 className="heading-secondary">Interests</h2>
-          <p className="text-secondary">
-            Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
-            Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
-          </p>
-        </div>
+        <About />
+        <Interests />
       </div>
     </div>
   )
---
+++ src/components/Interests.jsx
@@ -0,0 +1,11 @@
+export default function Interests() {
+  return (
+    <div className="description flex-column">
+      <h2 className="heading-secondary">Interests</h2>
+      <p className="text-secondary">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  )
+}
